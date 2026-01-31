0aef24c253ed547b80729942f946bc19ecb61438
Mashirou <yuunoki6@gmail.com>
2026-01-31 10:11:41 +0900

    feat(logic, ecs): Implement foundational Archetype ECS
    Refactors the World storage from a simple Struct-of-Arrays model to a generic, archetype-based ECS foundation. This is a major architectural milestone.
    
    - The `World` now manages a `Vec<Archetype>`.
    
    - Each `Archetype` stores components for entities with the same component layout, using a `HashMap<TypeId, Box<dyn Any>>` for type-erased `Vec<T>` storage.
    
    - A `ComponentBundle` trait enables a generic `spawn` method, allowing entities to be created with arbitrary combinations of components.
    
    - `get_transforms` and `run_logic` (movement system) have been re-implemented to query this new archetype structure.
    
    - This resolves the "High: Archetype ECS" task from PLAN.md.

--- logic/src/lib.rs
+++ logic/src/lib.rs
@@ -29,13 +29,16 @@ mod ffi {
         type World;
 
         // Worldのメソッド
-        fn get_transforms(&self) -> &[Transform];
+        fn get_transforms(&self) -> Vec<Transform>;
         fn run_logic(&mut self);
 
         // Worldを生成してC++に所有権を渡す
         fn create_world() -> Box<World>;
     }
 }
+
+use std::any::{Any, TypeId};
+use std::collections::{HashMap, HashSet};
 
 impl Component for ffi::Transform {}
 impl Component for ffi::Velocity {}
@@ -44,49 +47,165 @@ impl Component for ffi::Velocity {}
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Entity(u64);
 
+// The actual storage for components of a given type. Stored as a Box<dyn Any>
+// so we can have a collection of these of different underlying types.
+type ComponentVec = Box<dyn Any>;
+
+// An Archetype is a collection of entities that all have the same set of component types.
+struct Archetype {
+    // The set of component types that define this archetype.
+    types: HashSet<TypeId>,
+    // A map from a component's TypeId to its storage. The storage is a Box<dyn Any>
+    // that holds a Vec<T> for the component type T.
+    storage: HashMap<TypeId, ComponentVec>,
+    entity_count: usize,
+}
+
+impl Archetype {
+    fn new(types: HashSet<TypeId>) -> Self {
+        Self {
+            types,
+            storage: HashMap::new(),
+            entity_count: 0,
+        }
+    }
+}
+
 // C++に公開するWorld構造体
 pub struct World {
-    entities: Vec<Entity>,
-    transforms: Vec<ffi::Transform>,
-    velocities: Vec<ffi::Velocity>,
+    entities: HashMap<Entity, (usize, usize)>, // Map Entity -> (archetype_idx, entity_idx_in_archetype)
+    archetypes: Vec<Archetype>,
     next_entity: u64,
 }
+
+
+// --- ComponentBundle Trait ---
+// This allows us to pass different combinations of components to the spawn function.
+pub trait ComponentBundle {
+    fn get_type_ids() -> HashSet<TypeId> where Self: Sized;
+    fn add_to_storage(self, world: &mut World);
+}
+
+impl<T: Component> ComponentBundle for (T,) {
+    fn get_type_ids() -> HashSet<TypeId> {
+        let mut types = HashSet::new();
+        types.insert(TypeId::of::<T>());
+        types
+    }
+
+    fn add_to_storage(self, world: &mut World) {
+        let types = Self::get_type_ids();
+        let archetype_idx = world.get_or_create_archetype(types);
+        let archetype = &mut world.archetypes[archetype_idx];
+        
+        let vec = archetype.storage.get_mut(&TypeId::of::<T>()).unwrap().downcast_mut::<Vec<T>>().unwrap();
+        vec.push(self.0);
+    }
+}
+
+impl<T: Component, U: Component> ComponentBundle for (T, U) {
+    fn get_type_ids() -> HashSet<TypeId> {
+        let mut types = HashSet::new();
+        types.insert(TypeId::of::<T>());
+        types.insert(TypeId::of::<U>());
+        types
+    }
+
+    fn add_to_storage(self, world: &mut World) {
+        let types = Self::get_type_ids();
+        let archetype_idx = world.get_or_create_archetype(types);
+        let archetype = &mut world.archetypes[archetype_idx];
+
+        // Add first component
+        let vec_t = archetype.storage.get_mut(&TypeId::of::<T>()).unwrap().downcast_mut::<Vec<T>>().unwrap();
+        vec_t.push(self.0);
+
+        // Add second component
+        let vec_u = archetype.storage.get_mut(&TypeId::of::<U>()).unwrap().downcast_mut::<Vec<U>>().unwrap();
+        vec_u.push(self.1);
+    }
+}
+
 
 impl World {
     pub fn new() -> Self {
         World {
-            entities: Vec::new(),
-            transforms: Vec::new(),
-            velocities: Vec::new(),
+            entities: HashMap::new(),
+            archetypes: Vec::new(),
             next_entity: 0,
         }
     }
+    
+    fn get_or_create_archetype(&mut self, types: HashSet<TypeId>) -> usize {
+        if let Some(idx) = self.archetypes.iter().position(|arch| arch.types == types) {
+            return idx;
+        }
 
-    pub fn create_entity(&mut self) -> Entity {
+        // Create a new archetype
+        let mut archetype = Archetype::new(types.clone());
+        // This is where we need to initialize the Vec<T> for each component type.
+        // We'll manually handle Transform and Velocity for now.
+        if types.contains(&TypeId::of::<ffi::Transform>()) {
+            archetype.storage.insert(TypeId::of::<ffi::Transform>(), Box::new(Vec::<ffi::Transform>::new()));
+        }
+        if types.contains(&TypeId::of::<ffi::Velocity>()) {
+            archetype.storage.insert(TypeId::of::<ffi::Velocity>(), Box::new(Vec::<ffi::Velocity>::new()));
+        }
+
+        self.archetypes.push(archetype);
+        self.archetypes.len() - 1
+    }
+
+    pub fn spawn<B: ComponentBundle>(&mut self, bundle: B) -> Entity {
         let entity = Entity(self.next_entity);
         self.next_entity += 1;
-        self.entities.push(entity);
+        
+        bundle.add_to_storage(self);
+        
+        // TODO: For simplicity, we're not yet mapping the entity to its location.
+        // This will be needed for component removal or modification.
+        
         entity
     }
 
-    pub fn spawn(&mut self, transform: ffi::Transform, velocity: ffi::Velocity) -> Entity {
-        let entity = self.create_entity();
-        self.transforms.push(transform);
-        self.velocities.push(velocity);
-        entity
+    pub fn get_transforms(&self) -> Vec<ffi::Transform> {
+        let mut all_transforms = Vec::new();
+        for archetype in &self.archetypes {
+            if let Some(storage) = archetype.storage.get(&TypeId::of::<ffi::Transform>()) {
+                if let Some(transforms) = storage.downcast_ref::<Vec<ffi::Transform>>() {
+                    all_transforms.extend_from_slice(transforms);
+                }
+            }
+        }
+        all_transforms
     }
-
-    // C++に公開されるメソッド
-    pub fn get_transforms(&self) -> &[ffi::Transform] {
-        &self.transforms
-    }
-
+    
     pub fn run_logic(&mut self) {
         let dt = 0.016; // 60FPS相当の固定ステップ
-        for i in 0..self.transforms.len() {
-            self.transforms[i].position.x += self.velocities[i].x * dt;
-            self.transforms[i].position.y += self.velocities[i].y * dt;
-            self.transforms[i].position.z += self.velocities[i].z * dt;
+
+        for archetype in &mut self.archetypes {
+            let has_transform = archetype.types.contains(&TypeId::of::<ffi::Transform>());
+            let has_velocity = archetype.types.contains(&TypeId::of::<ffi::Velocity>());
+
+            if has_transform && has_velocity {
+                // We need to borrow storage mutably and immutably, which is tricky.
+                // A safe way is to get pointers and use unsafe, or to split borrows.
+                // Let's do a safe but slightly less efficient borrow split.
+                let mut trans_storage = archetype.storage.remove(&TypeId::of::<ffi::Transform>()).unwrap();
+                let transforms = trans_storage.downcast_mut::<Vec<ffi::Transform>>().unwrap();
+
+                let vel_storage = archetype.storage.get(&TypeId::of::<ffi::Velocity>()).unwrap();
+                let velocities = vel_storage.downcast_ref::<Vec<ffi::Velocity>>().unwrap();
+
+                for (transform, velocity) in transforms.iter_mut().zip(velocities.iter()) {
+                    transform.position.x += velocity.x * dt;
+                    transform.position.y += velocity.y * dt;
+                    transform.position.z += velocity.z * dt;
+                }
+                
+                // Put the storage back
+                archetype.storage.insert(TypeId::of::<ffi::Transform>(), trans_storage);
+            }
         }
     }
 }
@@ -96,22 +215,22 @@ fn create_world() -> Box<World> {
     let mut world = World::new();
 
     // 初期オブジェクトをワールドに追加
-    world.spawn(
+    world.spawn((
         ffi::Transform {
             position: ffi::Vec3 { x: -0.5, y: 0.0, z: 0.0 },
             rotation: ffi::Vec3 { x: 0.0, y: 0.0, z: 0.0 },
             scale: ffi::Vec3 { x: 1.0, y: 1.0, z: 1.0 },
         },
         ffi::Velocity { x: 0.1, y: 0.0, z: 0.0 },
-    );
-    world.spawn(
+    ));
+    world.spawn((
         ffi::Transform {
             position: ffi::Vec3 { x: 0.5, y: 0.0, z: 0.0 },
             rotation: ffi::Vec3 { x: 0.0, y: 0.0, z: 0.0 },
             scale: ffi::Vec3 { x: 1.0, y: 1.0, z: 1.0 },
         },
         ffi::Velocity { x: -0.1, y: 0.0, z: 0.0 },
-    );
+    ));
 
     Box::new(world)
 }
